f66a53755238f2c5933371ec099ff3cbc9c8a3f3
Mikal Reed-Larsen <585124@stud.hvl.com>
2023-09-04 13:48:46 +0200

    Ansatt klassen

--- DAT108/Oblig1/src/oppg2/Ansatt.java
+++ DAT108/Oblig1/src/oppg2/Ansatt.java
@@ -1,5 +1,66 @@
 package oppg2;
 
+
+
 public class Ansatt {
+
+	String fornavn;
+	String etternavn;
+	Kjønn kjønn;
+	String stilling;
+	int aarslonn;
+	
+	public Ansatt(String fornavn, String etternavn, Kjonn kjonn, String stilling, int aarslonn) {
+		this.fornavn = fornavn;
+		this.etternavn = etternavn;
+		this.kjønn = kjonn;
+		this.stilling = stilling;
+		this.aarslonn = aarslonn;
+	}
+	
+	public String getFornavn(String fornavn) {
+		return fornavn;
+	}
+	
+	public void setFornavn() {
+		this.fornavn = fornavn;
+	}
+	
+	public String getEtternavn(String etternavn) {
+		return etternavn;
+	}
+	
+	public void setEtternavn() {
+		this.etternavn = etternavn;
+	}
+	
+	public Kjønn getKjonn(Kjønn kjonn) {
+		return kjonn;
+	}
+	
+	public void setKjønn() {
+		this.kjønn = kjønn;
+	}
+	
+	public String getStilling(String stilling) {
+		
+		return stilling;
+	}
+	public void setStilling() {
+		this.stilling = stilling;
+	}
+	
+	
+	public int getAarslonn(int aarslonn) {
+		return aarslonn;
+	}
+	
+	public void setAarslonn() {
+		this.aarslonn = aarslonn;
+	}
+	
+	public String toString() {
+		return fornavn + ", " + etternavn + ", " + kjønn + ", " + stilling + ", " + aarslonn; 
+	}
 
 }
